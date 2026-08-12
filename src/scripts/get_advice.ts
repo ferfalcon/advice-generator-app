663e2b28371467300e05028c3718f2a42c912eb9
Fer <fer.falcon.dg@gmail.com>
2026-08-12 01:53:29 -0300

    feat: enhance advice display and improve styling for better user experience

--- src/scripts/get_advice.ts
+++ src/scripts/get_advice.ts
@@ -2,8 +2,8 @@ import { adviceIdElement, adviceElement } from "./consts";
 import writeElement from "./write_element";
 
 export default async function getAdvice(url: string) {
-  const response = await fetch(url);
   try {
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
